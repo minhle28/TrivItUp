f28b9cfd578b299a07701b26c657b8813e7930d1
Rijul_Mrhta <mehtarijul9@gmail.com>
2023-12-03 19:14:32 -0500

    Added user points functionality to backend

--- app/src/main/java/com/example/trivitup/ResultActivity.java
+++ app/src/main/java/com/example/trivitup/ResultActivity.java
@@ -2,13 +2,20 @@ package com.example.trivitup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.trivitup.databinding.ActivityResultBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ResultActivity extends AppCompatActivity {
     ActivityResultBinding binding;
-    int POINTS = 10;
+    FirebaseFirestore database;
+    long POINTS = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +25,23 @@ public class ResultActivity extends AppCompatActivity {
         int correctAnswers = getIntent().getIntExtra("correct",0);
         int totalQuestions = getIntent().getIntExtra("total",0);
 
-        int points = correctAnswers * POINTS;
+        long points = correctAnswers * POINTS;
         binding.score.setText(String.format("%d/%d",correctAnswers,totalQuestions));
+        binding.pointsEarned.setText(String.valueOf(points));
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection("users")
+                .document(FirebaseAuth.getInstance().getUid())
+                .update("points", FieldValue.increment(points));
 
     }
+
+    public void onClick(View view){
+        int viewId = view.getId();
+
+        if (viewId == R.id.restartBtn) {
+            Intent intent = new Intent(ResultActivity.this,MainActivity.class);
+            startActivity(intent);}
+
+        }
 }
